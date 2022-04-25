881bc294c72fc11ea7e141e27a4d9a9ea4b1a8b7
Andrii Stets <andrii.stets@e-hps.com>
2022-04-25 21:17:12 +0300

    60-61. Adding a sorting specification part

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -8,6 +8,7 @@ namespace Core.Specifications
 {
     public class BaseSpecification<T> : ISpecification<T>
     {
+        private ISpecification<T> _specificationImplementation;
         public BaseSpecification() {}
 
         public BaseSpecification(Expression<Func<T, bool>> criteria)
@@ -20,9 +21,21 @@ namespace Core.Specifications
         public List<Expression<Func<T, object>>> Includes {get;} = 
             new List<Expression<Func<T,object>>>();
 
+        public Expression<Func<T, object>> OrderBy { get; private set; }
+        public Expression<Func<T, object>> OrderByDescending { get; private set; }
+
         protected void AddInclude(Expression<Func<T,object>> includeExpression)
         {
             Includes.Add(includeExpression);
+        }
+
+        protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
+        {
+            OrderBy = orderByExpression;
+        }
+        protected void AddOrderByDescending(Expression<Func<T, object>> orderByDescExpression)
+        {
+            OrderByDescending = orderByDescExpression;
         }
     }
 }
